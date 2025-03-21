38f1cefcf5c7af1872c5c5265f255919b44caef8
nekena123 <rakotomanjatonekena@gmail.com>
2025-03-21 15:44:20 +0300

    remove

--- Models/TestModel.cs
+++
@@ -1,9 +0,0 @@
-// Models/TestModel.cs
-namespace CRMC_.Models
-{
-    public class TestModel
-    {
-        public string Message { get; set; }
-        public DateTime CurrentDate { get; set; }
-    }
-}
