e21a609a0019de1df47b522efab9d48b7d813733
nekena123 <rakotomanjatonekena@gmail.com>
2025-03-21 15:38:07 +0300

    initialize

---
+++ Models/TestModel.cs
@@ -0,0 +1,9 @@
+// Models/TestModel.cs
+namespace CRMC_.Models
+{
+    public class TestModel
+    {
+        public string Message { get; set; }
+        public DateTime CurrentDate { get; set; }
+    }
+}
